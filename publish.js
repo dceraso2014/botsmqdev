84b5241c2197231c490ac3fa47b0f6bfcb80984c
Diego Ceraso <dceraso@smartqube.com.ar>
2018-05-21 14:02:18 -0300

    (master)

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../botsmq.zip');
+var kuduApi = 'https://botsmq.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$botsmq';
+var password = 'SmFc4xTFR68F5w6rPNRyXsuggQicChFqwolm3HQiEnl54QBEuAldhobeTxQs';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('botsmq publish');
+  } else {
+    console.error('failed to publish botsmq', err);
+  }
+});
